c9254e799365470574991951a5dfb9f9412dad0b
Issanui <128009226+Issanui@users.noreply.github.com>
2025-04-28 09:47:42 +0000

    Update utils.py

--- Dashboard/utils.py
+++ Dashboard/utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 
 def apply_custom_css():
-    with open("style.css") as f:
+    with open("Dashboard/style.css") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 def load_data_app1():
